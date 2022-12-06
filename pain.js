fd1c2587338616fa63983346e7234db7ab7cecb7
BrendanAdamskiMSU <adamsk43@msu.edu>
2022-12-06 12:23:27 -0500

    added the other stuff

--- pain.js
+++ pain.js
@@ -5,12 +5,20 @@ function changeColor(){
     var scrollValue = window.scrollY;
     var background = document.getElementById("background")
     var bean = document.getElementById("bean")
-    var bean2 = document.getElementById("bean2")
+    var secondBrendan = document.getElementById("bean2")
+    var link1 = document.getElementById("bean3")
+    var link2 = document.getElementById("bean4")
+    var link3 = document.getElementById("bean5")
+
+
     if (scrollValue <= 400) {
         background.classList.add("background1")
         background.classList.remove('background2')
         bean.classList.add("size1")
         bean.classList.remove("size2")
+        link1.classList.remove("size69")
+        link2.classList.remove("size70")
+        link3.classList.remove("size71")
 
 
     }
@@ -20,20 +28,48 @@ function changeColor(){
         background.classList.remove('background3')
         bean.classList.add("size2")
         bean.classList.remove("size1")
-        bean2.classList.add("size3")
-        bean2.classList.remove("size4")
+        secondBrendan.classList.add("size3")
+        secondBrendan.classList.remove("size4")
+        link1.classList.remove("size69")
+        link2.classList.remove("size70")
+        link3.classList.remove("size71")
+        link1.classList.add("sizemid")
+        link2.classList.add("sizemid")
+        link3.classList.add("sizemid")
+
 
     } if (scrollValue > 800){
         bean.classList.add("size2")
         bean.classList.remove("size1")
-        bean2.classList.add("size4")
-        bean2.classList.remove("size3")
+        secondBrendan.classList.add("size4")
+        secondBrendan.classList.remove("size3")
+        link1.classList.add("size69")
+        link2.classList.add("size70")
+        link3.classList.add("size71")
+        link1.classList.remove("sizemid")
+        link2.classList.remove("sizemid")
+        link3.classList.remove("sizemid")
+        link1.classList.remove("colorWhite")
+        link2.classList.remove("colorWhite")
+        link3.classList.remove("colorWhite")
+        secondBrendan.classList.remove("colorWhite")
+
+
     }
-    if (scrollValue >= 2100){
+    if (scrollValue >= 1900){
         bean.classList.add("size2")
         bean.classList.remove("size1")
         background.classList.add("background3")
         background.classList.remove('background2')
+        link1.classList.add("colorWhite")
+        link2.classList.add("colorWhite")
+        link3.classList.add("colorWhite")
+        secondBrendan.classList.add("colorWhite")
+}   if(scrollValue >= 3400){
+    link1.classList.remove("colorWhite")
+    link2.classList.remove("colorWhite")
+    link3.classList.remove("colorWhite")
+    secondBrendan.classList.remove("colorWhite")
 }
 }
 
